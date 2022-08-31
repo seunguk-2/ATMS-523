ce6f1a337eaa5b791e6263d17800d3c07c86d5b4
SeungUk Kim <ksu1949@gmail.com>
2022-08-31 16:47:54 -0500

    added get_last_digit() to Module1.py

--- Module1.py
+++ Module1.py
@@ -1,9 +1,9 @@
-def get_last_two_digit(seed):
-    out = (seed*661231616) % 100
+def get_last_digit(seed):
+    out = (seed*661231616) % 10
 
     return out
 
 seed_num = input("Enter 2-digit number: ")
-return_num = get_last_two_digit(int(seed_num))
+return_num = get_last_digit(int(seed_num))
 
 print(return_num)
